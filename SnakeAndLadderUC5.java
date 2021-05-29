03224335c33467ff1fd04a9943cd44ec6d8dd162
Sujoyroy10 <sujoyroy.619@gmail.com>
2021-05-29 19:59:08 +0530

    ADDED:SnakeAndLadderUC5 code

---
+++ SnakeAndLadderUC5.java
@@ -0,0 +1,40 @@
+
+public class SnakeAndLadderUC5 {
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO THE SNAKE AND LADDER GAME!!");
+		int diceRoll = 0;
+		int position = 0;
+		System.out.println("ORIGINAL PLAYER POSITION IS: " + position);
+		while (position < 100) {
+			int win = 100;
+			if (position < 0) {
+				position = 0;
+			}
+			diceRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+			System.out.println("DICE ROLLED AND YOU HAVE: " + diceRoll);
+			System.out.println("NEW POSITION OF PLAYER IS: " + position);
+			int option = (int) (Math.floor(Math.random() * 10) % 3 + 1);
+			switch (option) {
+			case 1:
+				System.out.println("PLAYER HAS PASSED HIS CHANCE");
+				System.out.println("POSITION AFTER PASS: " + position);
+				break;
+			case 2:
+				System.out.println("PLAYER GOT A LADDER!!");
+				if ((position + diceRoll) > win) {
+					position = position - diceRoll;
+					break;
+				} else {
+					position = position + diceRoll;
+					System.out.println("POSITION AFTER LADDER: " + position);
+					break;
+				}
+			case 3:
+				System.out.println("PLAYER GOT A SNAKE");
+				position = position - diceRoll;
+				System.out.println("POSITION AFTER SNAKE: " + position);
+				break;
+			}
+		}
+	}
+}

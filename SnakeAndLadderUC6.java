63dacba5a965af0468efc67b80ee4c43bc43d4ed
Sujoyroy10 <sujoyroy.619@gmail.com>
2021-05-29 19:59:44 +0530

    ADDED:SnakeAndLadderUC6 code

---
+++ SnakeAndLadderUC6.java
@@ -0,0 +1,42 @@
+public class SnakeAndLadderUC6 {
+	public static void main(String[] args) {
+		System.out.println("WELCOME TO THE SNAKE AND LADDER GAME!!");
+		int diceRoll = 0;
+		int position = 0;
+		int numDiceRolls = 0;
+		System.out.println("ORIGINAL PLAYER POSITION IS: " + position);
+		while (position < 100) {
+			int win = 100;
+			if (position < 0) {
+				position = 0;
+			}
+			diceRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+			numDiceRolls++;
+			System.out.println("DICE ROLLED AND YOU HAVE: " + diceRoll);
+			System.out.println("New Position of player is: " + position);
+			int option = (int) (Math.floor(Math.random() * 10) % 3 + 1);
+			switch (option) {
+			case 1:
+				System.out.println("PLAYER HAS PASSED HIS CHANCE!!");
+				System.out.println("Position after Pass: " + position);
+				break;
+			case 2:
+				System.out.println("PLAYER GOT A LADDER!!");
+				if ((position + diceRoll) > win) {
+					position = position - diceRoll;
+					break;
+				} else {
+					position = position + diceRoll;
+					System.out.println("Position after Ladder: " + position);
+					break;
+				}
+			case 3:
+				System.out.println("PLAYER GOT A SNAKE!!");
+				position = position - diceRoll;
+				System.out.println("Position after Snake: " + position);
+				break;
+			}
+		}
+		System.out.println("Total number of Dice Rolls required to win is: " + numDiceRolls);
+	}
+}

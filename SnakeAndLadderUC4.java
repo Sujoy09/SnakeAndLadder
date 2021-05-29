5e0f7b02bcb4646d605a11b0e756b53cdce0c995
Sujoyroy10 <sujoyroy.619@gmail.com>
2021-05-29 19:57:23 +0530

    ADDED:SnakeAndLadderUC4 code

---
+++ SnakeAndLadderUC4.java
@@ -0,0 +1,41 @@
+
+public class SnakeAndLadderUC4 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Snake & Ladder Game !!");
+
+		int position = 0;
+		System.out.println("The player Position is: " + position);
+		while (position <= 100)
+		{
+			if (position < 0)
+			{
+				position = 0;
+			}
+			int diceRoll = (int) (Math.floor(Math.random() * 10 ) % 6 + 1);
+			System.out.println("Dice Roll: " + diceRoll);
+			position = position + diceRoll;
+			System.out.println("New Position of Player is: " + position);
+			int option = (int) (Math.floor(Math.random() * 10 ) % 3 + 1);
+			switch (option)
+			{
+				case 1:
+					System.out.println("No Play");
+					System.out.println("Position: " + position);
+					break;
+				case 2:
+					System.out.println("Ladder");
+					position = position + diceRoll;
+					System.out.println("Position: " + position);
+					break;
+				case 3:
+					System.out.println("Snake");
+					position = position - diceRoll;
+					System.out.println("Position: " + position);
+					break;
+			}
+		}
+	}
+
+
+	}
